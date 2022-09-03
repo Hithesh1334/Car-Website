ddc8cd67135f4b44abbf03933871bd864a97f6e5
Hithesh1334 <hiteshg1334@gmail.com>
2022-09-03 22:36:00 +0530

    css for the home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,22 +3,22 @@ import React from 'react';
 const Home = () => {
     return (
         <>
-        <div className="home-body">
-           <div class="container">
-                <div class="row">
-                    <div class="col-md-12 text-center">
-                        <h3 class="animate-charcter"> WELCOME!</h3>
-                    </div>
-                </div>
-            </div>
+        <div className= " homepage">
+            <div className="home-body">
             <div class="container">
-                <div class="row">
-                    <div class="col-md-12 text-center">
-                        <h3 class="animate-charcter"> THIS IS A WEB DEV PAGE</h3>
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <h3 class="animate-charcter"> WELCOME TO THE VIRTUAL SHOWROOM!</h3>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <h3 class="animate-charcter2"> WELCOME TO THE VIRTUAL SHOWROOM!</h3>
+                        </div>
                     </div>
                 </div>
             </div>
-            </div>
+        </div>    
         </>
     );
 }
